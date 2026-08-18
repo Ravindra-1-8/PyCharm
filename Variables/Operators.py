3a50788157cdb6325e3cdd22152a7b9578ee38a9
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-18 15:43:00 +0530

    Operators.py

---
+++ Variables/Operators.py
@@ -0,0 +1,10 @@
+a = 15
+b = 4
+print("Division:",a/b)
+print("Multiplication:",a*b)
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Floor Division:",a/b)
+print("Modulo:",a%b)
+print("exponentiation:",a**b)
+print("Pow:",a**b)
