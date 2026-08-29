10b08286119b190b87d058b758cd3be42a51942b
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-29 18:23:14 +0530

    operators.py

---
+++ strings/operators.py
@@ -0,0 +1,17 @@
+my_fav_numbers = 10
+your_fav_number = my_fav_numbers
+
+Ope = my_fav_numbers is your_fav_number
+print(Ope)
+
+bui = id(my_fav_numbers)
+print(bui)
+
+ipo = id(your_fav_number)
+print(ipo)
+
+inu = my_fav_numbers is not your_fav_number
+print(inu)
+
+noi = bui is not ipo
+print(noi)
